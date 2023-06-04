35300758024e63a35bbc843684bfa8f066085b0c
Serah-Njenga <serah35.nn@gmail.com>
2023-06-04 14:33:12 +0300

    compl

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -1,3 +1,26 @@
-class Audition < ActiveRecord::Base
+class CreateAuditions < ActiveRecord::Migration[6.1]
+
+    belongs_to :role
+
+    def change
+      create_table :auditions do |t|
+        t.string :actor
+        t.string :location
+        t.integer :phone
+        t.boolean :hired, default: false
+        t.references :role, foreign_key: true
   
-end
+        t.timestamps
+      end
+    end
+
+    def role
+        Role.find_by(id: role_id)
+      end
+      
+      def call_back
+        update(hired: true)
+      end
+      
+  end
+  
